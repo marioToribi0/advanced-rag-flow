1e48fcea495d7def2ee75371b2fdac738c03a97e
Mario Toribio <yuniormtr@gmail.com>
2024-11-10 16:24:43 -0400

    feat: generate-node

---
+++ graph/nodes/generate.py
@@ -0,0 +1,17 @@
+from graph.state import GraphState
+
+from typing import Any, Dict
+from graph.chains.generation import generation_chain
+
+def generate(state: GraphState) -> Dict[str, Any]:
+    print("---GENERATE---")
+    
+    question = state["question"]
+    documents = state['documents']
+    
+    generation = generation_chain.invoke({
+        "context": documents,
+        "question": question
+    })
+    
+    return {"documents": documents, "question": question, "generation": generation}
